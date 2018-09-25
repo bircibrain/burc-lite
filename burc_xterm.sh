2a39c921eff16c3ea45cb9eb9088faa80a08c8b4
Roeland Hancock <rhancock@gmail.com>
2018-09-24 22:10:59 -0400

    Fix container name

--- burc_xterm.sh
+++ burc_xterm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run --read-only -it -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix burc-lite /bin/bash 
+docker run --read-only -it --rm -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix rhancock/burc-lite /bin/bash 
 xhost - $IP
